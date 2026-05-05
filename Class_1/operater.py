63d0bc027c1de8a73cf08e93a279b4123ac25080
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-05-05 09:31:22 +0530

    Practice

---
+++ Class_1/operater.py
@@ -0,0 +1,35 @@
+# Arithmetic Operators
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+#Assignment Operators
+x = 5
+
+print(x)
+
+# Comparison Operators
+
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#Logical Operators
+x = 5
+
+print(x > 0 and x < 10)
+
+
