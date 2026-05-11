ada4caa54a2deaa302dcbfeb6ce296fee04155dd
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-05-11 22:40:35 +0530

    Python for devOps

---
+++ Class_1/11_may_class.py
@@ -0,0 +1,13 @@
+# Nasted For Loops
+
+# for i in range (1,6):
+#     print(i,"First Loops")
+#     print("_"*100)
+#     for j in range(1,6):
+#         print(j,"inner loops")
+        
+
+emp_name = ["","",""]
+for i in emp_name():
+    print(i)
+
---
+++ Class_1/11_may_practice.py
@@ -0,0 +1,9 @@
+# m= int(input("Enter your number :- "))
+
+# for i in range (1,m,1):
+#     print(i, end=" ")
+
+# What is list comprehension
+
+# num = [x*x for x in range(5) if x%2==0]
+# print(num)
