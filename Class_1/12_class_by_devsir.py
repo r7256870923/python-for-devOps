c566355ba67dddb424b11255434a624a447972dd
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-05-12 15:45:39 +0530

    12 may class

---
+++ Class_1/12_class_by_devsir.py
@@ -0,0 +1,83 @@
+# name = "Python"
+# for i in (name):
+#     print(i, end=" ")
+
+
+# Traversing of string using range()
+
+# name1 = "Python Programing"
+# size = len(name1)
+
+# for i in range(size):
+#     print(name1[0],i)
+
+# Without range()
+
+# name2= "python"
+# for i in name2:
+#     print(i)
+
+# without range( ) variable
+# for i in "Python":
+#     print(i)
+
+
+# var1= "DevOps Engineer"
+
+# for i in var1:
+#     if i=="e" or i=="E":
+#         continue
+    # print(i,end=" ")
+
+# var2= "this is devops batch"
+# # a= ("a","e","i","o","u")
+# for i in var2:
+#     if i=="a" or i=="e" or i=="i" or i=="o" or i=="u" :
+#         continue
+#     print(i, end=" ")
+
+# str1= "This is devops batch"
+# v_count=0
+# c_count = 0
+# for i in str1:
+#     if i in "aeiouAEIOU":
+#         v_count+=1
+#     else:
+#         c_count+=1
+# print(v_count)
+# print(c_count)
+
+# WAT to print your name in reverce formate
+
+# var4= "Rohit"
+# r_v=""
+# for i in var4:
+#     r_v=i+r_v
+# print(r_v)
+
+# v="ro"
+# r= ""
+# for i in v:
+#     r = i+r
+#     print(r)
+# # "" = r + ""
+# # r = o + r
+# # or
+# # print(r)
+
+# v= "python"
+# size = len(v)
+# for i in range (size):
+#     print(v,i)
+
+v= "python"
+size = len(v)
+for i in range(size):
+    print(v[i],v,i)
+
+
+
+
+
+
+
